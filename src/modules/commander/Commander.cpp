0082c1847face0a1add047df362509a5a88e753e
benerone <f.bighelli@gmail.com>
2021-05-21 09:40:17 +0200

    fix bug ini variable

--- src/modules/commander/Commander.cpp
+++ src/modules/commander/Commander.cpp
@@ -3263,6 +3263,7 @@ Commander::update_control_mode()
 	case vehicle_status_s::NAVIGATION_STATE_ACRO:
 		control_mode.flag_control_manual_enabled = true;
 		control_mode.flag_control_rates_enabled = true;
+		control_mode.flag_control_attitude_enabled = false;
 		break;
 
 	case vehicle_status_s::NAVIGATION_STATE_DESCEND:
